46326c44eb349fd55f06957019d7aa889d9849b8
jon-mag-santos <jon.mag.santos@gmail.com>
2025-05-28 09:47:34 +0200

    Game start

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
+  constructor(private router:Router) {}
 
+  newGame() {
+    this.router.navigateByUrl('/game');
+  }
 }
