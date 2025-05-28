7c8f6799a1043e29546b3af18c7f76451f26dec6
jon-mag-santos <jon.mag.santos@gmail.com>
2025-05-28 21:15:37 +0200

    Show correct card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -12,18 +12,36 @@ import { Game } from '../../models/game';
 export class GameComponent implements OnInit {
   cardStack = [1, 2, 3, 4];
   pickCardAnimation = false;
-  game: Game | undefined;
+  playedCardAnimation = false;
+  game: Game = new Game;
+  currentCard :string | undefined;
+  playedCard :string | undefined;
 
   ngOnInit(): void {
     this.newGame();
   }
 
-  takeCard(isTaken:boolean){
-    this.pickCardAnimation = isTaken;
-  }
-
   newGame() {
     this.game = new Game();
-    console.log(this.game);
+    console.log(this.game, this.currentCard);
   }
+
+  takeCard(isTaken:boolean){
+    if(!this.playedCardAnimation)
+      this.playedCardAnimation = true;
+
+    if(!this.pickCardAnimation) {
+      this.pickCardAnimation = isTaken;
+      this.currentCard = this.game.stack.pop();
+
+      setTimeout(() => {
+          this.playedCard = this.currentCard;
+          this.game.playedCard.push(this.playedCard); 
+          setTimeout(() => {
+            this.pickCardAnimation = false;
+        }, 150);
+      }, 1500);
+    }
+  }
+
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,13 +1,12 @@
 export class Game {
     public players: string[] = [];
     public stack: string[] = [];
-    public playedCard: string[] = [];
+    public playedCard: any [] = [];
     public currentPlayer: number = 0;
 
     constructor() {
         for (let i = 1; i < 14; i++) {
-            this.stack.push('ace_' + i);
-            this.stack.push('spade_' + i);
+            this.stack.push('spades_' + i);
             this.stack.push('hearts_' + i);
             this.stack.push('clubs_' + i);
             this.stack.push('diamonds_' + i);
